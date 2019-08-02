7944dd2aa5a991d0bcdb4bdff7da5226b246cb03
Kaile Su <s2179956@staff.ad.griffith.edu.au>
2019-08-02 15:58:12 +1000

    test

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+// need to import NgForm and Router
 
 @Component({
   selector: 'app-login',
